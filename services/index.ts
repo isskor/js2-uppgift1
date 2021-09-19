197637c1e0c36fa3982d752f5b4172258a56d214
jerry <jphuong91@gmail.com>
2021-09-19 20:12:17 +0200

    hardcode baseurl

--- services/index.ts
+++ services/index.ts
@@ -1,42 +1,46 @@
 import axios from 'axios';
 
 const key = process.env.API_KEY;
-const endpoint = process.env.ENDPOINT;
-
-axios.defaults.baseURL = endpoint;
 
 export type TMovieTypes = 'now_playing' | 'popular' | 'top_rated';
 export type TPage = number;
 
 const moviesUrl = (type: TMovieTypes, page: number) =>
-  `/movie/${type}/?api_key=${key}&page=${page}`;
+  `https://api.themoviedb.org/3/movie/${type}/?api_key=${key}&page=${page}`;
 
 export const getNowPlaying = (page: number = 1) =>
   axios(moviesUrl('now_playing', page));
 
 export const getPopular = (timeWindow?: 'week' | 'day', page: number = 1) =>
-  axios(`/trending/movie/${timeWindow}?api_key=${key}&page=${page}`);
+  axios(
+    `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${key}&page=${page}`
+  );
 
 export const getTopRated = (page: number = 1) =>
   axios(moviesUrl('top_rated', page));
 
-export const getGenres = () => axios(`/genre/movie/list?api_key=${key}`);
+export const getGenres = () =>
+  axios(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`);
 
 export const getMoviesByGenre = (genre: number, page: number = 1) =>
-  axios(`/discover/movie?api_key=${key}&page=${page}&with_genres=${genre}`);
+  axios(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${key}&page=${page}&with_genres=${genre}`
+  );
 
 export const getMovieDetail = (id: number | string) =>
   axios(
-    `/movie/${id}?api_key=${key}&append_to_response=similar,casts,images,videos`
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&append_to_response=similar,casts,images,videos`
   );
 
 export const getActor = (id: number | string) =>
-  axios(`/person/${id}?api_key=${key}&append_to_response=credits`);
+  axios(
+    `https://api.themoviedb.org/3/person/${id}?api_key=${key}&append_to_response=credits`
+  );
 
 export const getMovieCasts = (id: number | string) =>
-  axios(`/movie/${id}/casts?api_key=${key}`);
+  axios(`https://api.themoviedb.org/3/movie/${id}/casts?api_key=${key}`);
 
 export const getSearchedMovies = (query: string, page: number = 1) =>
   axios(
-    `/search/movie?api_key=ec88e2e0aead7f05f85d68d6d49c7f1b&query=${query}&page=${page}`
+    `https://api.themoviedb.org/3/search/movie?api_key=ec88e2e0aead7f05f85d68d6d49c7f1b&query=${query}&page=${page}`
   );
